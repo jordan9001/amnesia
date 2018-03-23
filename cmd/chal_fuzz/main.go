899df104dd5521177915e3fc914c47a5c4fd67d6
Jordan Whitehead <jordan9001@gmail.com>
2018-03-23 14:03:32 -0600

    Got initial fuzzing working, reworked the structure

--- cmd/chal_fuzz/main.go
+++ cmd/chal_fuzz/main.go
@@ -2,8 +2,108 @@ package main
 
 import (
 	"github.com/jordan9001/amnesia"
+	"io"
+	"log"
+	"math/rand"
+	"strings"
+	"sync"
+	"syscall"
+	"time"
 )
 
+var counter int = 0
+var counter_mux *sync.Mutex = &sync.Mutex{}
+
+var charset []byte = []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!.,?")
+
+func trySeq(stdin io.WriteCloser, stdout, stderr io.ReadCloser, result chan amnesia.Hit, status chan *syscall.WaitStatus, quit chan struct{}, args []string) {
+	// basic send brute force sequence of readable-characters
+	counter_mux.Lock()
+	c := counter
+	counter++
+	counter_mux.Unlock()
+
+	in := make([]byte, 0, 1024)
+
+	for c > 0 {
+		chr := charset[c%len(charset)]
+		c = c / len(charset)
+		in = append(in, chr)
+	}
+
+	stdin.Write(in)
+
+	out := make([]byte, 1024)
+	n, _ := stdout.Read(out)
+
+	s := string(out[:n])
+
+	if strings.Contains(strings.ToLower(s), "flag") {
+		// got a flag hit!
+		h := amnesia.Hit{
+			Kind:   "Flag",
+			Input:  string(in),
+			Output: s,
+		}
+		result <- h
+	}
+}
+
+const max_size int = 8
+
+func tryRand(stdin io.WriteCloser, stdout, stderr io.ReadCloser, result chan amnesia.Hit, status chan *syscall.WaitStatus, quit chan struct{}, args []string) {
+	// basic send of random readablecharacters
+	size := rand.Intn(max_size-1) + 1
+	in := make([]byte, size)
+	for i := 0; i < size; i++ {
+		in[i] = charset[rand.Intn(len(charset))]
+	}
+	stdin.Write(in)
+	out := make([]byte, 1024)
+	n, _ := stdout.Read(out)
+
+	s := string(out[:n])
+
+	if strings.Contains(strings.ToLower(s), "flag") {
+		// got a flag hit!
+		h := amnesia.Hit{
+			Kind:   "Flag",
+			Input:  string(in),
+			Output: s,
+		}
+		result <- h
+	}
+}
+
 func main() {
+	var ctx amnesia.Context
+
+	ctx.WorkerCount = 15
+	ctx.BufferSize = 15
+	ctx.Timeout = time.Second * 6
+
+	var quitchan chan struct{} = nil
+	var args = []string{}
+
+	resseq, _ := amnesia.Fuzz(ctx, "./chal", args, trySeq, quitchan)
+	resrand, _ := amnesia.Fuzz(ctx, "./chal", args, tryRand, quitchan)
+
+	for i := 0; i < 45; i++ {
+		select {
+		case h := <-resseq:
+			log.Printf("Got a seq hit of type %q!\n", h.Kind)
+			if h.Kind == "Flag" {
+				log.Printf("\tInput %q\n", h.Input.(string))
+				log.Printf("\tFlag! %q\n", h.Output.(string))
+			}
+		case h := <-resrand:
+			log.Printf("Got a rand hit of type %q!\n", h.Kind)
+			if h.Kind == "Flag" {
+				log.Printf("\tInput %q\n", h.Input.(string))
+				log.Printf("\tFlag! %q\n", h.Output.(string))
+			}
+		}
+	}
+
 	return
 }
---
+++ fuzz.go
@@ -0,0 +1,154 @@
+package amnesia
+
+import (
+	"context"
+	"io"
+	"log"
+	"os/exec"
+	"syscall"
+	"time"
+)
+
+type Context struct {
+	WorkerCount int
+	BufferSize  int
+	Timeout     time.Duration
+}
+
+// used for reporting a crash, find, etc
+// should include all necessary info to repeat the crash
+type Hit struct {
+	Kind   string
+	Args   []string
+	Input  interface{}
+	Output interface{}
+}
+
+// ArgFunc should generate command line arguments to be passed
+type ArgFunc func() []string
+
+// CommFunc will communicate with the program
+// CommFunc has final say about when to make and send on a Hit
+// send hit on result
+// get termination result on status
+// quit early if quit closes
+type CommFunc func(stdin io.WriteCloser, stdout, stderr io.ReadCloser, result chan Hit, status chan *syscall.WaitStatus, quit chan struct{}, args []string)
+
+// I donno if I should have quit be returned or passed in?
+
+func FuzzArgs(ctx Context, path string, args ArgFunc, comm CommFunc, quit chan struct{}) (results chan Hit, err error) {
+	return fuzz(ctx, path, args, comm, quit)
+}
+
+func Fuzz(ctx Context, path string, args []string, comm CommFunc, quit chan struct{}) (results chan Hit, err error) {
+	return fuzz(ctx, path, args, comm, quit)
+}
+
+func fuzz(ctx Context, path string, args interface{}, comm CommFunc, quit chan struct{}) (results chan Hit, err error) {
+	// This function starts all the workers
+
+	// check for valid arguments
+	switch args.(type) {
+	case ArgFunc:
+	case []string:
+	default:
+		log.Fatal("Invalid Args")
+	}
+
+	if ctx.WorkerCount <= 0 {
+		log.Fatal("Invalid Context WorkerCount")
+	}
+
+	if ctx.BufferSize < 0 {
+		log.Fatal("Invalid Context BufSize")
+	}
+
+	// make the results chan we will pass back to the caller
+	// buffer the result chan
+	results = make(chan Hit, ctx.BufferSize)
+
+	for i := 0; i < ctx.WorkerCount; i++ {
+		go fuzzWorker(ctx, path, args, comm, results, quit)
+	}
+
+	return results, nil
+}
+
+func fuzzWorker(ctx Context, path string, args interface{}, comm CommFunc, result chan Hit, quit chan struct{}) {
+	var loop bool = true
+	for loop {
+		// this function loops running a command, and sends back any hits
+		var cmd *exec.Cmd
+
+		var strargs []string
+		switch v := args.(type) {
+		case ArgFunc:
+			strargs = v()
+		case []string:
+			strargs = v
+		}
+
+		if ctx.Timeout > 0 {
+			timectx, _ := context.WithTimeout(context.Background(), ctx.Timeout)
+			cmd = exec.CommandContext(timectx, path, strargs...)
+		} else {
+			cmd = exec.Command(path, strargs...)
+		}
+
+		stdin, err := cmd.StdinPipe()
+		if err != nil {
+			log.Fatal(err)
+		}
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			log.Fatal(err)
+		}
+		stderr, err := cmd.StderrPipe()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		retchan := make(chan *syscall.WaitStatus, 1)
+
+		go comm(stdin, stdout, stderr, result, retchan, quit, strargs) // start the fuzzer function
+
+		err = cmd.Start()
+		if err != nil {
+			log.Fatalf("cmd.Start : %v\n", err)
+		}
+
+		var status *syscall.WaitStatus
+		status = nil
+
+		err = cmd.Wait() // block until program finishes or fails
+
+		if err != nil {
+			if exerr, ok := err.(*exec.ExitError); ok {
+				// here we get into platform dependent stuff
+				// Windows only has a WaitStatus with an ExitCode
+				// but that exit code says a lot about what happened
+				// Linux has a WaitStatus that implements stuff
+				st, ok := exerr.Sys().(syscall.WaitStatus)
+				if !ok {
+					log.Fatalf("Couldn't asert syscall.WaitStatus type\n")
+				}
+				status = &st
+			} else {
+				log.Fatalf("cmd.Wait : %v\n", err)
+			}
+		}
+		//  else program finished successfully, and we pass on a nil WaitStatus
+
+		// by this time the CommFunc could have finished, so that is why retchan needs a buffer
+		retchan <- status
+
+		// check if we should stop
+		select {
+		case <-quit:
+			// we are done
+			return
+		default:
+			// keep going
+		}
+	}
+}
