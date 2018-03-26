d9dc31430ce72353c362e20db0bb85ee05444488
jordan9001 <jordan9001@gmail.com>
2018-03-26 16:54:03 -0600

    added BLNS

--- mutator.go
+++ mutator.go
@@ -1,6 +1,7 @@
 package amnesia
 
 import (
+	"encodeing/base64"
 	"math/rand"
 )
 
@@ -11,14 +12,30 @@ type HEntry struct {
 
 // table that balances the amount we do each operation
 var hTable []HEntry = []HEntry{
+	{1, bad_add},
+	{1, bad_replace},
+	{5, rearrange_random},
+	{9, remove_random},
 	{15, add_random},
 	{9, replace_random},
+	{3, repeat_add},
+	{4, repeat_replace},
 }
 var hTableTotal int
 
 func init() {
 	// initialize hTableTotal
 	setHTableTotal()
+	// initialize BLNS from BLNS_b64
+
+	BLNS = make([][]byte, len(BLNS_b64))
+	for i, v := range BLNS_b64 {
+		bad, err := base64.StdEncoding.DecodeString(v)
+		if err != nil {
+			log.Fatalf("Error while decoding BLNS_b64 entry %d\n", i)
+		}
+		BLSN[i] = bad
+	}
 }
 
 func setHTableTotal() {
@@ -57,11 +74,35 @@ func Mutate(src string, level int) string {
 
 // change operations TODO
 // - find ascii numbers and mess them up
-// - add known naughty strings
+
+func bad_add(in []byte) []byte {
+	bad := BLNS[rand.Intn(len(BLNS))]
+	sz := len(bad)
+	pos := rand.Intn(len(in) + 1)
+
+	// insert
+	out := append(in, make([]byte, sz))
+	copy(out[pos+sz:], in[pos:])
+	copy(out[pos:], bad)
+}
+
+func bad_replace(in []byte) []byte {
+	bad := BLNS[rand.Intn(len(BLNS))]
+	if len(in) < len(bad) {
+		// silent fail
+		return in
+	}
+
+	pos := rand.Intn(len(in) - len(bad) + 1)
+
+	copy(in[pos:], bad)
+
+	return in
+}
 
 func rearrange_random(in []byte) []byte {
 	pos := rand.Intn(len(in))
-	sz := rand.Intn(len(in) - pos - 1) + 1
+	sz := rand.Intn(len(in)-pos-1) + 1
 
 	dst := rand.Intn(len(in) + 1 - sz)
 	tblock := make([]byte, sz)
@@ -108,14 +149,14 @@ func repeat_add(in []byte) []byte {
 	// insert
 	out := append(in, make([]byte, size))
 	copy(out[pos+size:], in[pos:])
-	copy(out[pos:pos+size], in[start:end])
+	copy(out[pos:], in[start:end])
 	return out
 }
 
 func repeat_replace(in []byte) []byte {
 	// take a random selection
 	pos := rand.Intn(len(in))
-	sz := rand.Intn(len(in) - pos - 1) + 1
+	sz := rand.Intn(len(in)-pos-1) + 1
 
 	dst := rand.Intn(len(in) + 1 - sz)
 
